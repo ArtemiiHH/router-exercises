30979e56f786e547ae4a0e99e2283477ac14f0b4
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-01-31 13:12:34 +0200

    Play around with routes

---
+++ src/routes.jsx
@@ -0,0 +1,16 @@
+import App from "./App";
+import Profile from "./Profile";
+import ErrorPage from "./ErrorPage";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+];
+
+export default routes;
